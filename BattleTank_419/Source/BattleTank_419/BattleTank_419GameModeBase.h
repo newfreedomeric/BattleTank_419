84f3ee3b16f3316a8dd8a02ddb4e3c721ee11c02
Eric Harrington <newfreedomeric@gmail.com>
2018-11-01 16:25:02 -0400

    Initial Commit

---
+++ BattleTank_419/Source/BattleTank_419/BattleTank_419GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_419GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_419_API ABattleTank_419GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
